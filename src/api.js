46d70f6c699145a44c54993ac7d39f88c316b160
baliram123giri <gir71401@gmail.com>
2022-03-07 17:50:58 +0530

    newchanges

--- src/api.js
+++ src/api.js
@@ -3,17 +3,12 @@ const serverless = require("serverless-http");
 const user = require("./router/user")
 const app = express();
 
-app.use("/api", user)
+app.use("/.netlify/functions/api", user)
 app.use(express.json())
-const router = express.Router();
+// const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({
-    hello: "hi!"
-  });
-});
 
-app.use(`/.netlify/functions/api`, router);
+// app.use(`/.netlify/functions/api`, router);
 
 module.exports = app;
 module.exports.handler = serverless(app);
